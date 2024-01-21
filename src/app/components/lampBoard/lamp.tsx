daefcd9337d57e9bbf2a5aeeee29e19125abbac4
PunithG360 <punithg@figmatic.co>
2024-01-21 06:07:56 +0530

    fix: remove console log and add touch events

--- src/app/components/lampBoard/lamp.tsx
+++ src/app/components/lampBoard/lamp.tsx
@@ -1,17 +1,24 @@
 'use client'
 import useKeyPress from '@/app/hooks/keyDetect'
 import { TLamp } from './utils'
+import { useState } from 'react'
 
 const Lamp: React.FC<{ letter: TLamp }> = ({ letter }) => {
   const keyPress = useKeyPress(letter)
-  if (letter === 'q') console.log(keyPress)
+  const [touch, setTouch] = useState(false)
+  const active = keyPress || touch
   return (
     <div
       className={`flex flex-col justify-end items-end 
       w-16 h-20 p-3 border-2 rounded-xl text-3xl 
-      border-primary
-      ${keyPress ? 'text-secondary bg-primary' : 'text-primary bg-secondary'}
+      border-primary select-none
+      ${active ? 'text-secondary bg-primary' : 'text-primary bg-secondary'}
 `}
+      onMouseDown={() => setTouch(true)}
+      onTouchStart={() => setTouch(true)}
+      onMouseLeave={() => setTouch(false)}
+      onMouseUp={() => setTouch(false)}
+      onTouchEnd={() => setTouch(false)}
     >
       <div>{letter}</div>
     </div>
